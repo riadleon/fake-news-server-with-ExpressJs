9d7f96c7f0d0a090f3696de1da59a4e7dd2b6844
Riad Shalahin Leon <105214634+riadleon@users.noreply.github.com>
2022-10-20 15:49:00 +0600

    Basic backend server practising with express js

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const categories = require('./data/categories.json')
+
+
+app.get('/', (req, res) => {
+    res.send('News API running')
+})
+
+app.get('/categories', (req, res) => {
+    res.send(categories);
+})
+
+
+app.listen(port, () => {
+    console.log(`Fake News Server listening on port ${port}`)
+})
