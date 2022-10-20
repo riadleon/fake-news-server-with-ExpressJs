836d873a1872f8ddb7fa2acdea277897d3d4af15
Riad Shalahin Leon <105214634+riadleon@users.noreply.github.com>
2022-10-20 20:54:37 +0600

    fetched data

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./data/categories.json')
+const news = require('./data/news.json')
 
 
 app.get('/', (req, res) => {
@@ -16,6 +17,26 @@ app.get('/categories', (req, res) => {
     res.send(categories);
 })
 
+
+//lot of categories id thats why filter
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news);
+    }
+    else {
+        const category_news = news.filter(n => n.category_id === id)
+        res.send(category_news);
+    }
+
+})
+
+//id ektai thats why find
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id)
+    res.send(selectedNews);
+})
 
 app.listen(port, () => {
     console.log(`Fake News Server listening on port ${port}`)
